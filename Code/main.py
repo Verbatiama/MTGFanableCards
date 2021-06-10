a481e14a67dd1b12b3ea3bc80b2880cb1cc2cc84
Adam H <Verbatiama@gmail.com>
2021-06-10 20:01:25 +0930

    Word wrap prototype

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,42 @@
 from PIL import Image, ImageDraw, ImageFont
-im = Image.open("Assets\\Card\\acard.png").convert('RGBA')
+import time
+#from . import helper
+
+#work out imports
+def break_text(txt, font, max_width):
+
+    # We share the subset to remember the last finest guess over 
+    # the text breakpoint and make it faster
+    subset = len(txt)
+    letter_size = None
+
+    text_size = len(txt)
+    while text_size > 0:
+
+        # Let's find the appropriate subset size
+        while True:
+            width, height = font.getsize(txt[:subset])
+            letter_size = width / subset
+            
+            #TODO stop loop
+            # min/max(..., subset +/- 1) are to avoid looping infinitely over a wrong value
+            if width < max_width and text_size >= subset: # Too short
+                subset = max(int(max_width * subset / width), subset + 1)
+            elif width > max_width: # Too large
+                subset = min(int(max_width * subset / width), subset - 1)
+                break
+            else: # Subset fits, we exit
+                break
+
+        yield txt[:subset]
+        txt = txt[subset:]   
+        text_size = len(txt)
+
+def fontMaxHeight(font):
+    width, height = font.getsize("".join(list(map(chr, range(32, 123)))))
+    return height
+
+im = Image.open("Assets\\Card\\acard.jpg").convert('RGBA')
 # box = (400, 400, 800, 800)
 # box2 = (800, 600)
 # region = im.crop(box)
@@ -17,7 +54,14 @@ d = ImageDraw.Draw(txt)
     # draw text, half opacity
 d.text((30,25), "A Magic Card Name", font=fnt, fill=(0,0,0,255))
     # draw text, full opacity
-d.text((30,320,10,400), "All the text that goes in a text box", font=fnt, fill=(0,0,0,255))
+#d.text((30,320,10,400), "All the text that goes in a text box", font=fnt, fill=(0,0,0,255))
+
+fnt = ImageFont.truetype("Assets\\Fonts\\Beleren Small Caps.ttf",10)
+
+height = fontMaxHeight(fnt)
+
+for i, line in enumerate(break_text("".join(list(map(chr, range(32, 123))))*2, fnt, 300)):
+    d.text((30, 330+height*i), line, (0,0,0,255), font=fnt)   
 
 out = Image.alpha_composite(im, txt)
 
